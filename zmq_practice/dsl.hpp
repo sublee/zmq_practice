bbfe1e1d8553a840cda127aa253f960cb740136a
Heungsub Lee <h@subl.ee>
2013-01-22 18:57:32 +0900

    init repo

---
+++ zmq_practice/dsl.hpp
@@ -0,0 +1,23 @@
+#include "boost/thread.hpp"
+
+// from http://ricanet.com/new/view.php?id=blog/050807 and http://ricanet.com/new/view.php?id=blog/050811a
+class AutoLock
+{
+public:
+    AutoLock(boost::detail::basic_timed_mutex& mutex) : _mutex(mutex)
+    {
+		_mutex.lock();
+    }
+    ~AutoLock()
+    {
+		_mutex.unlock();
+    }
+	operator bool()
+	{
+		return false;
+	}
+private:
+    boost::detail::basic_timed_mutex& _mutex;
+};
+
+#define synchronized(x) if (AutoLock __ = x) assert(0); else
